726675af12778eed3df2c78ba906b5e594284284
MakarosVar <49597451+MakarosVar@users.noreply.github.com>
2025-02-16 16:21:08 +0200

    fixes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -34,10 +34,11 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(401, 'Invalid email or password'));
     }
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password: userPassword, ...userInfo } = user._doc;
     res
       .cookie('access_token', token, { http: true })
       .status(200)
-      .json(user);
+      .json(userInfo);
   } catch (error) {
     console.error('Error in signin:', error);
     next(errorHandler(500, error.message));
