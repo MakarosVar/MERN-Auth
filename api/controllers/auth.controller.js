8d17fe81a727b77431af07f93a224a08e0af47e8
MakarosVar <49597451+MakarosVar@users.noreply.github.com>
2025-02-16 16:08:15 +0200

    sign in backend

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -16,6 +17,29 @@ export const signup = async (req, res, next) => {
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
     console.error('Error in signup:', error);
+    next(errorHandler(500, error.message));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const isMatch = bcryptjs.compareSync(password, user.password);
+    if (!isMatch) {
+      return next(errorHandler(401, 'Invalid email or password'));
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res
+      .cookie('access_token', token, { http: true })
+      .status(200)
+      .json(user);
+  } catch (error) {
+    console.error('Error in signin:', error);
     next(errorHandler(500, error.message));
   }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signup, signin } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
@@ -9,6 +9,6 @@ router.post('/signup', (req, res, next) => {
   signup(req, res, next);
 });
 
-router.post('/signin', (req, res) => {});
+router.post('/signin', signin);
 
 export default router;
